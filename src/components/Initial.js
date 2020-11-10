c2df345176c5be3d2b6ba590fbcfc0ce9a598349
kermit1013 <k84101311@gmail.com>
2020-11-10 22:57:52 +0800

    added initial page

---
+++ src/components/Initial.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Carousel } from 'antd';
+
+const contentStyle = {
+    height: '600px',
+    color: '#fff',
+    lineHeight: '600px',
+    textAlign: 'center',
+    background: '#364d79',
+  };
+class Initial extends React.Component {
+  render() {
+    return (
+        <Carousel autoplay>
+        <div>
+          <h3 style={contentStyle}>1</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>2</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>3</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>4</h3>
+        </div>
+      </Carousel>
+    
+    )
+  }
+}
+
+export default Initial
