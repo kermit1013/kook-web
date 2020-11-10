03e4c70db8953e2e87b4862cd909539b28fc1461
kermit1013 <k84101311@gmail.com>
2020-11-11 00:19:48 +0800

    added AreaPrediction page

---
+++ src/components/AreaPrediction.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Card } from "antd";
+
+class AreaPrediction extends React.Component {
+  render() {
+    return (
+     <div></div>
+    );
+  }
+}
+
+export default AreaPrediction;
