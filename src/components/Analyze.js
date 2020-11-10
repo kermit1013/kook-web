2cc3f593aa2122e39e62b64f585b1febb99ddc03
kermit1013 <k84101311@gmail.com>
2020-11-10 22:58:20 +0800

    added prediction & analyze page

---
+++ src/components/Analyze.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Input} from 'antd';
+
+class Analyze extends React.Component {
+  render() {
+    return (
+      <div>
+        <Input placeholder="Basic usage"></Input>
+      </div>
+    )
+  }
+}
+
+export default Analyze
---
+++ src/components/Prediction.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Button} from 'antd';
+
+class Prediction extends React.Component {
+  render() {
+    return (
+      <div>
+        <Button>BUTTON</Button>
+      </div>
+    )
+  }
+}
+
+export default Prediction
