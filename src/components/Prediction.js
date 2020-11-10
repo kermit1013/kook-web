5bc923d0b9dbc01005c7a64003dd5ba3cde8f2c6
kermit1013 <k84101311@gmail.com>
2020-11-11 00:20:27 +0800

    prediction page set demo data

--- src/components/Prediction.js
+++ src/components/Prediction.js
@@ -1,14 +1,29 @@
-import React from 'react'
-import {Button} from 'antd';
+import React from "react";
+import { Card } from "antd";
 
 class Prediction extends React.Component {
   render() {
     return (
-      <div>
-        <Button>BUTTON</Button>
-      </div>
-    )
+      <Card title="西岸 West Spots">
+        <Card
+          type="inner"
+          title="外埔漁港 - 南堤"
+          extra={<a href="/area-prediction">More</a>}
+        >
+          左右跑、沙底、沙灘浪型，無鋒面時小小浪適合初學者衝的。
+退潮1-2小時才可以下水 或漲滿前，因為漲滿後會撞肉粽沙地很平坦，有浪時浪型都很好。鋒面來時會有黑龍。
+        </Card>
+        <Card
+          style={{ marginTop: 16 }}
+          type="inner"
+          title="外埔漁港 - 土地公"
+          extra={<a href="#">More</a>}
+        >
+          沙底、沙灘浪型。適合滿潮前後下水，太乾容易撞石頭，請勿亂停車影響附近居民
+        </Card>
+      </Card>
+    );
   }
 }
 
-export default Prediction
+export default Prediction;
